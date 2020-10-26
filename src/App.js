cd773e9bef1277f0e72bea31e747a6ad149d809b
Redhwan Nacef <nacefredhwan@gmail.com>
2020-10-26 15:43:49 +0000

    Add concept of children and props
    Slight refactor to the elements, mainly allowing children and props.
    This sets the foundation to allow adding elements to each other and also
    the props mainly allow styling for elements.

--- src/App.js
+++ src/App.js
@@ -4,30 +4,71 @@ import pluginTransformJsx from "@babel/plugin-transform-react-jsx";
 import prettier from "prettier";
 import babelParser from "prettier/parser-babel";
 
+import "./Utilities.css";
+
+const layoutElements = [
+  {
+    name: "container",
+    props: { className: "wrapper" },
+    type: "div",
+    children: [],
+  },
+];
+
+const htmlElements = [
+  { name: "div", type: "div", children: ["div"] },
+  { name: "h1", type: "h1", children: ["h1"] },
+  { name: "h2", type: "h2", children: ["h2"] },
+  { name: "h3", type: "h3", children: ["h3"] },
+  { name: "h4", type: "h4", children: ["h4"] },
+  { name: "p", type: "p", children: ["p"] },
+  { name: "button", type: "button", children: ["button"] },
+  { name: "input", type: "input", children: [] },
+];
+
 const transformJsx = (code) =>
   transform(code, { plugins: [pluginTransformJsx] }).code;
 
-const toComponent = (type, children) => {
-  return `<${type}>${children}</${type}>`;
+const formatProps = (props) => {
+  return Object.entries(props)
+    .map(([key, value]) => {
+      return `${key}={${JSON.stringify(value)}}`;
+    })
+    .join(" ");
+};
+
+const createElement = (type, props, children = []) => {
+  const formattedProps = props ? formatProps(props) : "";
+  return `<${type} ${formattedProps}>${children
+    .filter((obj) => Boolean(obj))
+    .map((child) =>
+      typeof child === "string"
+        ? child
+        : createElement(child.type, child.props, child.children)
+    )
+    .join("\n")}</${type}>`;
 };
 
 const DynamicComponent = ({ elements, raw }) => {
   const innerCode =
     elements.length > 0
       ? elements
-          .map(({ type, children }) => toComponent(type, children))
+          .map(({ type, props, children }) =>
+            createElement(type, props, children)
+          )
           .join("\n")
       : "";
+
+  if (raw)
+    return prettier.format(`const Code = () => (<>${innerCode}</>)`, {
+      parser: "babel",
+      plugins: [babelParser],
+    });
+
   const code = `return ${transformJsx(`<div>${innerCode}</div>`).replace(
     "\n",
     ""
   )}`;
-
-  if (raw)
-    return prettier.format(`<div>${innerCode}</div>`, {
-      parser: "babel",
-      plugins: [babelParser],
-    });
 
   const fn = new Function("React", code);
   return fn(React);
@@ -36,28 +77,45 @@ const DynamicComponent = ({ elements, raw }) => {
 const App = () => {
   const [elements, setElements] = useState([]);
 
-  const addElement = (element, children) =>
-    setElements((prevState) => [
-      ...prevState,
-      {
-        type: element,
-        children: children,
-      },
-    ]);
+  const addElement = (type, props, children) =>
+    setElements((prevState) => [...prevState, { type, props, children }]);
 
   return (
-    <div style={{ display: "flex", justifyContent: "space-around" }}>
-      <div>
-        <h1>App</h1>
-        <div style={{ display: "flex" }}>
-          <button onClick={() => addElement("h1", "Heading 1")}>H1</button>
-          <button onClick={() => addElement("h2", "Heading 2")}>H2</button>
-          <button onClick={() => addElement("h3", "Heading 3")}>H3</button>
-          <button onClick={() => addElement("p", "Paragraph")}>p</button>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-around",
+        padding: "0 50px",
+      }}
+    >
+      <div style={{ padding: "0 10px" }}>
+        <h1>Components</h1>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <h4>Layout Elements</h4>
+          {layoutElements.map(({ name, type, props, children }) => (
+            <button
+              key={name}
+              onClick={() => addElement(type, props, children)}
+            >
+              {name}
+            </button>
+          ))}
+          <h4>HTML Elements</h4>
+          {htmlElements.map(({ name, type, props, children }) => (
+            <button
+              key={name}
+              onClick={() => addElement(type, props, children)}
+            >
+              {name}
+            </button>
+          ))}
         </div>
+      </div>
+      <div style={{ padding: "0 10px", flexGrow: 2 }}>
+        <h1 align="center">App</h1>
         <DynamicComponent elements={elements} />
       </div>
-      <div>
+      <div style={{ padding: "0 10px" }}>
         <h1>Code</h1>
         <div style={{ fontFamily: "monospace", whiteSpace: "pre-wrap" }}>
           <DynamicComponent elements={elements} raw />
